3044112ddba827e19cd121b74cd69757c9f0af57
parsachow <parsakishwa06@gmail.com>
2023-07-13 10:35:52 -0400

    delete and update controllers

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,12 +1,43 @@
+const book = require('../models/book');
 const Book = require('../models/book');
 
 module.exports = {
     create,
-    deleteReview
+    deleteReview,
+    updateReview
 }
 
+async function updateReview(req, res){
+    try{
+        //findOneAndUpdate()???
+        const book = await Book.findOne({
+            'reviews._id': req.params.id,
+            'reviews.user': req.user._id
+        })
+        if(!book) return res.redirect('/books/index');
+        book.reviews.text = req.body.text;
+        await book.save();
+        res.redirect(`/books/${book._id}`)
+    }catch(err){
+        res.send(err);
+    }
+}
+
+
 async function deleteReview(req, res){
-    
+    try{
+        const book = await Book.findOne ({
+            'reviews._id': req.params.id,
+            'reviews.user': req.user._id
+        })
+
+        if(!book) return res.redirect('/books/index');
+        book.reviews.remove(req.params.id);
+        await book.save();
+        res.redirect(`/books/${book._id}`)
+    }catch(err){
+        res.send(err);
+    }
 }
 
 async function create(req, res){
